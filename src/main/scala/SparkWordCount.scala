b5c72ba51425529029cbf4066e7dad957d61e225
inhochoi <revaer@naver.com>
2017-08-07 22:21:18 +0900

    add spark word count

--- src/main/scala/SparkWordCount.scala
+++ src/main/scala/SparkWordCount.scala
@@ -1,3 +1,5 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SparkWordCount {
   def main(args: Array[String]): Unit = {
     SparkWordCountApplication.read
@@ -6,13 +8,19 @@ object SparkWordCount {
 
 object SparkWordCountApplication {
   val README_FILE_PATH = "/Users/inhochoi/Downloads/spark-2.2.0-bin-hadoop2.7/README.md"
-  val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+  val conf = new SparkConf().setAppName("Spark Word Count Class").setMaster("local[2]")
+  val sc = new SparkContext(conf)
 
   def read: Unit = {
-    val textFile = spark.read.textFile(README_FILE_PATH).cached()
-    val numA = textFile.filter(_.contains("a")).count()
-    val numB = textFile.filter(_.contains("b")).count()
-    println("A is $numA, B is $numB")
-    spark.stop()
+    val lines = sc.textFile(README_FILE_PATH)
+      .flatMap(line => line.split(" "))
+    val pairs = lines.map(s => (s, 1))
+    val counts = pairs.reduceByKey((a, b) => a+b)
+
+    counts.foreach(println)
+
+    val max = counts.sortByKey(true).first()
+
+    println(s"Max is $max")
   }
 }
