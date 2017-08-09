64f24ec3423ae7dcf73ff3d174a94426309ee235
william(최인호) <inhochoi@coupang.com>
2017-08-09 18:56:33 +0900

    Spark Word Count

--- src/main/scala/SparkWordCount.scala
+++ src/main/scala/SparkWordCount.scala
@@ -1,31 +1,43 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkWordCount {
   def main(args: Array[String]): Unit = {
-    SparkWordCountApplication.read
+    SparkWordCountApplication read
   }
 }
 
 object SparkWordCountApplication {
-  val README_FILE_PATH = "/Users/inho/Downloads/spark-2.2.0-bin-hadoop2.7/README.md"
-  val conf = new SparkConf().setAppName("Spark Word Count Class").setMaster("local[2]")
-  val sc = new SparkContext(conf)
+  val README_FILE_PATH: String = "/Users/inho/Downloads/spark-2.2.0-bin-hadoop2.7/README.md"
+  val conf: SparkConf = new SparkConf().setAppName("Spark Word Count Class").setMaster("local[2]")
+  val sc: SparkContext = new SparkContext(conf)
 
   def read: Unit = {
-    val lines = sc.textFile(README_FILE_PATH)
+    val lines: RDD[String] = sc.textFile(README_FILE_PATH)
       .flatMap(line => line.split(" "))
-    val pairs = lines.map(s => (s, 1))
-    val groupbyCount = pairs.reduceByKey((a, b) => a+b)
-    val letterCount = lines.map(_.length)
+    val pairs: RDD[(String, Int)] = lines.map(s => (s, 1))
+    val groupbyCount: RDD[(String, Int)] = pairs.reduceByKey((a, b) => a + b)
+    val letterCount: Double = lines.map(_.length)
       .sum()
 
     groupbyCount.foreach(println)
+    groupbyCount.foreachPartition(foreachMethod)
+    val objectRDD: RDD[KeyValueModel] = groupbyCount.map(convertToObject)
 
-    val max = groupbyCount.sortByKey(true).first()
+    objectRDD.foreach(println)
+    val max: (String, Int) = groupbyCount.sortByKey(true).first()
 
     println(s"Max is $max")
     println(s"Total length is $letterCount")
-
-    lines.saveAsTextFile("/tmp/test.log")
   }
+
+  def foreachMethod(input: Iterator[(String, Int)]) = {
+    for (a <- input) {
+    }
+  }
+
+  def convertToObject(input: (String, Int)) = KeyValueModel(input._1, input._2)
+
+  case class KeyValueModel(key: String, value: Int)
+
 }
